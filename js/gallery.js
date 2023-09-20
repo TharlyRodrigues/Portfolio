934df594ae51ba1b1947f1bf583743c85de40482
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-09-20 20:41:42 -0300

    update

---
+++ js/gallery.js
@@ -0,0 +1,18 @@
+var overlay = document.querySelector('.jl-overlay');
+var frameImage = document.querySelector(".jl-gallery-frame-image");
+var frameContainer = document.querySelector(".jl-gallery-frame-container");
+var galleryImages = document.querySelectorAll(".jl-thumb-img");
+
+const getImageSrc = function () {
+    for (var i = 0; i < galleryImages.length; i++) {
+        galleryImages[i].addEventListener("click", function () {
+            var imageSrc = this.getAttribute("data-src");
+            frameImage.setAttribute("src", imageSrc);
+
+            overlay.classList.toggle("jl-is-open");
+            frameContainer.classList.toggle("jl-is-open");
+        });
+    }
+}
+
+getImageSrc();
