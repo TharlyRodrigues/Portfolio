0fe886c05984ad45085a78e2c5f4a91f04badf06
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-10-23 11:40:06 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 //Declarando Variáveis
 var btnContact = document.querySelector(".jl-btn-contact");
 var toggleModal = document.querySelectorAll(".jl-toggle-modal");
+var toggleMenu = document.querySelectorAll(".jl-toggle-menu");
+var menuMobile = document.querySelectorAll(".jl-menu-mob");
+var overlay = document.querySelector(".jl-overlay");
+var btnManuMobIcon = document.querySelector(".jl-btn-menu-mob ion-icon");
 
 //Page Preloader
 window.addEventListener("load", function () {
@@ -19,23 +23,37 @@ btnContact.addEventListener("click", function () {
   this.classList.toggle("jl-change-icon");
 });
 
+// Abrindo e fechando o menu
+for (var m = 0; m < toggleMenu.length; m++) {
+  toggleMenu[m].addEventListener("click", function () {
+    var overlay = document.querySelector(".jl-menu-overlay");
+    var menuMobile = this.parentElement.querySelector(".jl-menu-mob");
+    overlay.classList.toggle("jl-is-open");
+    menuMobile.classList.toggle("jl-menu-is-closed");
+    menuMobile.classList.toggle("jl-menu-is-open");
+
+    var icon = btnManuMobIcon.getAttribute("name");
+
+    if (icon === "menu") {
+      btnManuMobIcon.setAttribute("name", "close");
+    } else btnManuMobIcon.setAttribute("name", "menu");
+  });
+}
 
 //Abrindo e Fechando o Modal de Orcamento
 for (var i = 0; i < toggleModal.length; i++) {
   toggleModal[i].addEventListener("click", function () {
     var overlay = document.querySelector(".jl-overlay");
     var modalOrcamento = document.querySelector("#jl-modal-orcamento");
-
     overlay.classList.toggle("jl-is-open");
     modalOrcamento.classList.toggle("jl-is-open");
     modalOrcamento.classList.toggle("jl-slide-top-in");
   });
 }
 
-var postGallery = document.querySelector('.jl-post-gallery');
+var postGallery = document.querySelector(".jl-post-gallery");
 var postGalleryHeight = postGallery.clientHeight;
-postGallery.style.height = (postGalleryHeight - 270) + 'px';
-
+postGallery.style.height = postGalleryHeight - 270 + "px";
 
 // Animando Elementos on Scroll com Waypoints
 var myScrollDown = document.querySelector(".jl-scroll-down");
