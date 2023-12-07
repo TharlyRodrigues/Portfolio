872caaeef17c0b72821f69b7da0fb997347fffaa
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-12-07 09:19:09 -0300

    animações na topbar em scroll

--- js/main.js
+++ js/main.js
@@ -50,3 +50,17 @@ for (var i = 0; i < toggleModal.length; i++) {
     modalOrcamento.classList.toggle("jl-slide-top-in");
   });
 }
+
+// Animando Elementos o=da topbar
+var triggerTopbar = document.querySelector(".jl-trigger-topbar");
+var topbar = document.querySelector(".jl-topbar");
+var logo = document.querySelector(".jl-logo");
+
+var waypoint = new Waypoint({
+  element: triggerTopbar,
+  handler: function () {
+    topbar.classList.toggle("jl-topbar-bg");
+    logo.classList.toggle("jl-logo-shorten");
+  },
+  offset: "50",
+});
